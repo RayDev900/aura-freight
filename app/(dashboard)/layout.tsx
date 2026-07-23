239868d06bd8838b91c4a2aa88aa07a29f1b5e09
RayDev <Raymedavid0@gmail.com>
2026-07-23 23:30:02 +0200

    feat: integrate framer-motion for animations, enhance Slidebar with role-based navigation, and update dashboard layout for session management

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,23 @@
-import Slidebar from "@/shared/components/Slidebar"
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import Sidebar from '@/shared/components/Slidebar'
 
-export default function DashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return <>
-    <Slidebar />
-    {children}
-    </>
-  }
+
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const session = await getServerSession()
+
+  console.log('SESSION:', JSON.stringify(session, null, 2))  // 
+  if (!session) redirect('/login')
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar role={session.user.role} />
+      <main className="flex-1 overflow-y-auto">{children}</main>
+    </div>
+  )
+}
